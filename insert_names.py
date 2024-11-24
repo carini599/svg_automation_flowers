156c5646126b67674fa5b3cd8c102a03a9a88cfb
Carina Breu <carina.cb.breu@gmail.com>
2024-11-24 16:22:30 +0100

    docs: add inline comments and logging infos

--- insert_names.py
+++ insert_names.py
@@ -1,44 +1,55 @@
 # %%
-from cairosvg import svg2png
 import pandas as pd
-
-# %%
+import logging
+import cv2
+import numpy as np
+import subprocess
 
 # Import Names from name_list.csv
 names = pd.read_csv('name_list.csv', sep=';', header=0)
+logging.info('Names imported from name_list.csv')
 
+# Replace NaN Values by empty string
 names['name']=names['name'].astype('string').fillna('')
-names.info()
+logging.info('NaN Values replaced by empty string to make not named flowers possible.')
 
 # %%
-import re
 
 # Copy template to new flower_24 file
 open('flowers_24_new.svg', 'w').write(open('flowers_24.svg').read())
 
-with open('flowers_24_new.svg') as f:
+
+# Change names in flower_24_new.svg
+with open('flowers_24_new.svg') as f: 
     data = f.read()
+
     for i in list(names['index']):
         index = names[names['index']==i]['index'].values[0]
-        if index<10:
+        if index<10: # index is smaller than 10 a zero is added before the index to get a two digit number as string
             index = '0'+ str(index)
         else:
             index = str(index)
 
+        # create reference string in the image which is in the form of 'name05' for example
         strtorepl = 'name' + index
+
+        # get name that shall replace the reference string
         name_i = names[names['index']==i]['name'].values[0]
-        print(name_i)
-        print(strtorepl)
+
+        # replcace placeholder with name from names list
         data = data.replace(strtorepl,name_i)
+        logging.info(f'{strtorepl} replaced by {name_i}.')
     
+    # save changes to the file
     with open('flowers_24_new.svg', 'w') as f:
         f.write(data)
 
+    logging.info('Names are changed and saved to flowers_24_new.svg')
+
 # %%
 
-import subprocess
+# open Inkscape to  save image to png
 
-#INKSCAPE_PATH = 'inkscape'  # for Linux
 INKSCAPE_PATH = r'C:\Program Files\Inkscape\bin\inkscape.exe'  # for Windows
 
 subprocess.run([
@@ -49,14 +60,14 @@ subprocess.run([
     'flowers_24_new.svg'
 ])
 
-# Invert Colors
+logging.info('PNG created from SVG.')
 
-import cv2
-import numpy as np
-
+# Invert Colors in png
 image = cv2.imread('flowers_24_new.png', 0)
 inverted = np.invert(image)
 
 cv2.imwrite('flowers_24_inv.jpg', inverted)
+
+logging.info('Inverted colors in PNG.')
 
 # %%
